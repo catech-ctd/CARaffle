19efa3653ce7fe512dc2610829335dd288c844bb
catech-ctd <marta.benach@ca.com>
2017-05-30 15:08:33 -0400

    Update CARaffle.js

--- protractor/CARaffle.js
+++ protractor/CARaffle.js
@@ -1,9 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://ondemand.saucelabs.com:80',
-  baseUrl: 'http://swat-preprod.cdbu.io/',
+  baseUrl: 'http://Pre-Production.8bjja9kb5t.us-east-2.elasticbeanstalk.com ',
   specs: ['spec.js'],
   onPrepare: function(){
-    browser.driver.get('http://swat-preprod.cdbu.io/');
+    browser.driver.get('http://Pre-Production.8bjja9kb5t.us-east-2.elasticbeanstalk.com/');
     element(by.linkText('About')).click();
     element(by.linkText('Home')).click();
     element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
