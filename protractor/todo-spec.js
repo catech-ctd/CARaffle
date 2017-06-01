46d696a69bb2ebdf8d3d81370591fa4b5833eba5
catech-ctd <marta.benach@ca.com>
2017-06-01 19:41:20 -0400

    Update todo-spec.js

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -3,6 +3,6 @@ describe('CA Raffle UI Test', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://Pre-Production.8bjja9kb5t.us-east-2.elasticbeanstalk.com/index.php');
     element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Enter Raffle')).click();
+    element(by.buttonText('Submit')).click();
     });
 });
