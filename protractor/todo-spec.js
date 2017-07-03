eb85a63343318b96c29b67aa85428a8fc2383b07
catech-ctd <marta.benach@ca.com>
2017-07-03 12:28:32 -0400

    Update todo-spec.js

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -1,8 +1,8 @@
-describe('CDBU Modern Software Factory UI Test - Swat Environment', function() {
+describe('CA Raffle UI Test - SWAT Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://swat-preprod.cdbu.io');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('I\'m In!')).click();
     });
 });
